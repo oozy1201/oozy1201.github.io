d38607772cc8cb4fabbfee85b345560945bf9ede
oozy1201 <66842688+oozy1201@users.noreply.github.com>
2021-07-01 19:50:34 +0900

    Update app.5e61588a.js

--- js/app.5e61588a.js
+++ js/app.5e61588a.js
@@ -87,7 +87,7 @@
                 r('iframe', {
                   attrs: {
                     /* 바꿔야 할곳 (트위치채널이름, 방송하는곳 도메인) */
-                    src: 'https://player.twitch.tv/?channel=acgn_music_box_01&parent=oozy1201.github.io',
+                    src: 'https://player.twitch.tv/?channel=nei0001&parent=oozy1201.github.io',
                     frameborder: '0',
                     allowfullscreen: 'true',
                     scrolling: 'no',
